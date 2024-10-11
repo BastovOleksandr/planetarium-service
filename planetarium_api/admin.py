ffe0d42f7836d05f18e109b6fb06efcbcc3fef4a
Olexandr Bastov <iamlucky1990@gmail.com>
2024-10-11 17:26:33 +0300

    feat: "planetarium_api" models registered in admin-panel
    Multiple tickets can be created on the order page

--- planetarium_api/admin.py
+++ planetarium_api/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
-# Register your models here.
+from planetarium_api.models import (
+    PlanetariumDome,
+    ShowTheme,
+    AstronomyShow,
+    ShowSession,
+    Reservation,
+    Ticket,
+)
+
+admin.site.register(PlanetariumDome)
+admin.site.register(ShowTheme)
+admin.site.register(AstronomyShow)
+admin.site.register(ShowSession)
+admin.site.register(Ticket)
+
+
+class TicketInLine(admin.TabularInline):
+    model = Ticket
+    extra = 1
+
+
+@admin.register(Reservation)
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (TicketInLine,)
