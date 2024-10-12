d60b5c0d485a3afe6c361289c9a0f7db6ebca42c
Olexandr Bastov <iamlucky1990@gmail.com>
2024-10-12 22:40:50 +0300

    chore: populate 'urls.py' for 'user' app

--- user/urls.py
+++ user/urls.py
@@ -5,10 +5,13 @@ from rest_framework_simplejwt.views import (
     TokenVerifyView,
 )
 
+from user.views import ManageUserView, CreateUserView
 
 app_name = "user"
 
 urlpatterns = [
+    path("register/", CreateUserView.as_view(), name="create"),
+    path("me/", ManageUserView.as_view(), name="manage"),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
