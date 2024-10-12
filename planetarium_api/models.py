448f1fadf05949562d5081d6cf78f1fe0f0f3517
Olexandr Bastov <iamlucky1990@gmail.com>
2024-10-12 13:24:23 +0300

    chore: add related_name to ManyToMany fields
    In "planetarium_api/models.py" for "Ticket" model
    Also minor changes in "__str__" methods

--- planetarium_api/models.py
+++ planetarium_api/models.py
@@ -8,14 +8,6 @@ from django.db.models.functions import Lower
 from django.utils.text import slugify
 
 
-def astronomy_show_image_file_path(instance: "AstronomyShow", filename):
-    _, extension = os.path.splitext(filename)
-    return os.path.join(
-        "uploads/astronomy_shows/",
-        f"{slugify(instance.title)}-{uuid.uuid4()}{extension}",
-    )
-
-
 class PlanetariumDome(models.Model):
     name = models.CharField(max_length=100)
     rows = models.IntegerField()
@@ -42,13 +34,19 @@ class ShowTheme(models.Model):
         return self.name
 
 
+def astronomy_show_image_file_path(instance: "AstronomyShow", filename):
+    _, extension = os.path.splitext(filename)
+    return os.path.join(
+        "uploads/astronomy_shows/",
+        f"{slugify(instance.title)}-{uuid.uuid4()}{extension}",
+    )
+
+
 class AstronomyShow(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     themes = models.ManyToManyField(ShowTheme, related_name="astronomy_shows")
-    image = models.ImageField(
-        null=True, upload_to=astronomy_show_image_file_path
-    )
+    image = models.ImageField(null=True, upload_to=astronomy_show_image_file_path)
 
     class Meta:
         ordering = (Lower("title"),)
@@ -60,35 +58,35 @@ class AstronomyShow(models.Model):
 class ShowSession(models.Model):
     show_time = models.DateTimeField()
     astronomy_show = models.ForeignKey(AstronomyShow, on_delete=models.CASCADE)
-    planetarium_dome = models.ForeignKey(
-        PlanetariumDome, on_delete=models.CASCADE
-    )
+    planetarium_dome = models.ForeignKey(PlanetariumDome, on_delete=models.CASCADE)
 
     class Meta:
         ordering = ("-show_time",)
 
     def __str__(self):
-        return f"{self.astronomy_show.title} at {str(self.show_time)}"
+        return f"{self.astronomy_show.title} at {self.show_time}"
 
 
 class Reservation(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
-    )
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     class Meta:
         ordering = ("-created_at",)
 
     def __str__(self):
-        return f"Reservation {self.id} by:\n{self.user}"
+        return f"Reservation by {self.user.email}. Created at: {self.created_at}"
 
 
 class Ticket(models.Model):
     row = models.IntegerField()
     seat = models.IntegerField()
-    show_session = models.ForeignKey(ShowSession, on_delete=models.CASCADE)
-    reservation = models.ForeignKey(Reservation, on_delete=models.CASCADE)
+    show_session = models.ForeignKey(
+        ShowSession, on_delete=models.CASCADE, related_name="tickets"
+    )
+    reservation = models.ForeignKey(
+        Reservation, on_delete=models.CASCADE, related_name="tickets"
+    )
 
     class Meta:
         unique_together = ("show_session", "row", "seat")
@@ -99,11 +97,7 @@ class Ticket(models.Model):
 
     @staticmethod
     def validate_row_seat(seat: int, row: int, show_session, error_to_raise):
-        for (
-            ticket_attr_value,
-            ticket_attr_name,
-            planetarium_dome_attr_name
-        ) in [
+        for ticket_attr_value, ticket_attr_name, planetarium_dome_attr_name in [
             (row, "row", "rows"),
             (seat, "seat", "seats_in_row"),
         ]:
@@ -137,5 +131,4 @@ class Ticket(models.Model):
         super().save(force_insert, force_update, using, update_fields)
 
     def __str__(self):
-        return (f"{str(self.show_session)}:\n"
-                f"(row: {self.row}, seat: {self.seat})")
+        return f"{self.show_session}:\n(row: {self.row}, seat: {self.seat})"
