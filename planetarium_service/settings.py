d851918e1f2ee3cc11cd29cdbb5c7797d8928597
Olexandr Bastov <iamlucky1990@gmail.com>
2024-10-13 23:15:15 +0300

    config: add throttle settings to REST framework

--- planetarium_service/settings.py
+++ planetarium_service/settings.py
@@ -149,6 +149,11 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle",
+    ],
+    "DEFAULT_THROTTLE_RATES": {"anon": "50/day", "user": "200/day"},
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
 
