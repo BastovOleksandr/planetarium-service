b3b36deff8c0110e717f9a74adc4a6f9ec2cfec7
Olexandr Bastov <iamlucky1990@gmail.com>
2024-10-13 13:10:15 +0300

    chore: include 'planetarium_api' urls to project 'urls.py'

--- planetarium_service/urls.py
+++ planetarium_service/urls.py
@@ -6,4 +6,8 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/user/", include("user.urls", namespace="user")),
+    path(
+        "api/planetarium-api/",
+        include("planetarium_api.urls", namespace="planetarium_api"),
+    ),
 ] + debug_toolbar_urls()
