b84992a09c33550e4296e7de13030138f13a4a5e
Olexandr Bastov <iamlucky1990@gmail.com>
2024-10-13 21:20:29 +0300

    docs: add 'drf_spectacular' and 'swagger' endpoints

--- planetarium_service/urls.py
+++ planetarium_service/urls.py
@@ -1,7 +1,10 @@
 from debug_toolbar.toolbar import debug_toolbar_urls
 from django.contrib import admin
 from django.urls import path, include
-
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -9,5 +12,11 @@ urlpatterns = [
     path(
         "api/planetarium-api/",
         include("planetarium_api.urls", namespace="planetarium_api"),
+    ),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/doc/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
     ),
 ] + debug_toolbar_urls()
